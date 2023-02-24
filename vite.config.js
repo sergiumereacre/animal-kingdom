291fe66a6cc196878e5f995d99d388e687155075
sergiu <sergiumer@gmail.com>
2023-02-24 18:30:18 +0000

    Vite config now works with laravel localhost.

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        }
+    },
 });
