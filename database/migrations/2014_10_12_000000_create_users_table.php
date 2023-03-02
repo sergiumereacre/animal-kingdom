cfb100ccd5cfda63fb81378ea0d3e00d615f55d4
Edison <20241135@studentmail.ul.ie>
2023-03-02 00:00:32 +0000

    tweaking order of migration, should be error free now

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,23 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id($column = 'user_id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->boolean('is_admin')->default(false);
+            $table->integer('species_id')->unsigned();
+            $table->foreign('species_id')->references('species_id')->on('animal_species');
+            $table->string('address')->nullable();
+            $table->date('date_of_birth');
+            $table->integer('organisation_id')->unsigned();
+            $table->foreign('organisation_id')->references('organisation_id')->on('organisations');
+            $table->string('contact_number')->nullable();
+            $table->boolean('is_banned')->default(false);
+            $table->text('bio')->nullable();
 
             // $table->increments('user_id');
             // $table->boolean('is_admin')->default(false);
