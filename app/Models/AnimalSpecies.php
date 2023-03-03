f750b387775d2267cf895f82f937ad1e4c0d6a99
TheCaister <edisoncai2001@gmail.com>
2023-03-03 20:43:18 +0000

    renaming files
    fixing animalspecy typo

--- app/Models/AnimalSpecies.php
+++ app/Models/AnimalSpecies.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AnimalSpecy extends Model
+class AnimalSpecies extends Model
 {
     use HasFactory;
 
--- database/factories/AnimalSpeciesFactory.php
+++ database/factories/AnimalSpeciesFactory.php
@@ -4,16 +4,16 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use App\Models\AnimalSpecy;
+use App\Models\AnimalSpecies;
 
-class AnimalSpecyFactory extends Factory
+class AnimalSpeciesFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = AnimalSpecy::class;
+    protected $model = AnimalSpecies::class;
 
     /**
      * Define the model's default state.
