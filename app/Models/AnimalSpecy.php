b82a967b1c46818ca583b75043a66b8ffe8b2a80
TheCaister <edisoncai2001@gmail.com>
2023-03-03 20:40:45 +0000

    generating models from blueprint

---
+++ app/Models/AnimalSpecy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AnimalSpecy extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'species_id',
+        'species_name',
+        'category',
+        'can_fly',
+        'can_swim',
+        'eating_style',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'can_fly' => 'boolean',
+        'can_swim' => 'boolean',
+    ];
+}
