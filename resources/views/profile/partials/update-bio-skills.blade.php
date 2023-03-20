7d118db7356fc6cfe018cdb4b64a6ffbf9af4c09
sergiu <sergiumer@gmail.com>
2023-03-20 22:44:42 +0000

    Quick commit.

--- resources/views/profile/partials/update-bio-skills.blade.php
+++ resources/views/profile/partials/update-bio-skills.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Bio and Skills') }}
+            {{ __('Bio, Skills and Qualifications') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile bio and skills that show up on your personal profile.") }}
+            {{ __("Update your account's profile bio, skills and qualifications that show up on your personal profile.") }}
         </p>
     </header>
 
@@ -25,6 +25,11 @@
             <x-input-error class="mt-2" :messages="$errors->get('skills')" />
         </div>
 
+        <div>
+            <x-input-label for="qualifications" :value="__('Qualifications')" />
+            <x-text-input id="qualifications" name="qualifications" type="text" class="mt-1 block w-full" />
+            <x-input-error class="mt-2" :messages="$errors->get('qualifications')" />
+        </div>
 
         <div class="flex items-center gap-4 justify-end">
             @if (session('status') === 'profile-updated')
