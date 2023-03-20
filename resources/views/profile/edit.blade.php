17d11c4a2ac87c83b2c9d72872d0ca9012f4ad8d
sergiu <sergiumer@gmail.com>
2023-03-20 14:18:39 +0000

    D

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -15,6 +15,12 @@
 
             <div class="p-8 bg-white shadow-xl md:rounded-3xl hover:shadow-2xl transition ease-in-out duration-300">
                 <div class="max-w-3xl">
+                    @include('profile.partials.update-bio-skills')
+                </div>
+            </div>
+
+            <div class="p-8 bg-white shadow-xl md:rounded-3xl hover:shadow-2xl transition ease-in-out duration-300">
+                <div class="max-w-3xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
---
+++ resources/views/profile/partials/update-bio-skills.blade.php
@@ -0,0 +1,43 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Bio and Skills') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600">
+            {{ __("Update your account's profile bio and skills that show up on your personal profile.") }}
+        </p>
+    </header>
+
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+        @csrf
+        @method('patch')
+
+        <div>
+            <x-input-label for="bio" :value="__('Bio')" />
+            <x-text-input id="bio" name="bio" type="text" class="mt-1 block w-full h-max" :value="old('bio', $user->bio)" required autofocus autocomplete="bio" />
+            <x-input-error class="mt-2" :messages="$errors->get('bio')" />
+        </div>
+
+        <div>
+            <x-input-label for="skills" :value="__('Skills')" />
+            <x-text-input id="skills" name="skills" type="text" class="mt-1 block w-full" />
+            <x-input-error class="mt-2" :messages="$errors->get('skills')" />
+        </div>
+
+
+        <div class="flex items-center gap-4 justify-end">
+            @if (session('status') === 'profile-updated')
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600"
+                >{{ __('Saved.') }}</p>
+            @endif
+            
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+        </div>
+    </form>
+</section>
