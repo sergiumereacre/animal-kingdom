2c5c3d55bd6c584fe3e2a84bdeb1d79ec1970f5d
sergiu <sergiumer@gmail.com>
2023-03-21 21:24:12 +0000

    organisation index done.

--- resources/views/organisations/index.blade.php
+++ resources/views/organisations/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="py-12 px-12 flex flex-col items-center md:flex-row md:justify-center gap-5 md:flex-wrap">
         @foreach ($organisations as $organisation)
-            <x-organisation-full :organisation="$organisation"></x-organisation-full>
+            <x-organisation-card :organisation="$organisation"></x-organisation-card>
         @endforeach
     </div>
 </x-app-layout>
