df81bb7d2bd3b7661348cf2e793aa5aac3895c1c
sergiu <sergiumer@gmail.com>
2023-03-21 22:54:32 +0000

    Connections fixes on profile.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/vacancies/index';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
