c1742bc97c970b00c5a5821f8abaa8b067a4a5ed
sergiu <sergiumer@gmail.com>
2023-03-22 00:12:05 +0000

    You can now easily tell who's admin.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,24 +17,40 @@
                 <div class="flex items-center pl-2">
                     <x-search-dropdown>
                         <x-slot name="trigger">
-                            <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-greenButtons hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-300 rounded-lg text-sm p-2.5 mr-1">
-                                <svg class="w-7 h-7" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                            <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
+                                aria-expanded="false"
+                                class="md:hidden text-greenButtons hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-300 rounded-lg text-sm p-2.5 mr-1">
+                                <svg class="w-7 h-7" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
                                 <span class="sr-only">Search</span>
                             </button>
                         </x-slot>
                         <x-slot name="content">
                             <div class="flex flex-row items-center gap-5">
-                                <x-text-input class="border border-greenButtons shadow-none w-64 h-8 rounded-2xl focus:outline-none focus:ring-0 pl-2"></x-text-input>
+                                <x-text-input
+                                    class="border border-greenButtons shadow-none w-64 h-8 rounded-2xl focus:outline-none focus:ring-0 pl-2">
+                                </x-text-input>
                                 <x-primary-button @click="open = false">Search</x-primary-button>
                             </div>
                         </x-slot>
                     </x-search-dropdown>
                     <div class="relative hidden md:block md:pl-5">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 md:pl-8 pointer-events-none">
-                            <svg class="w-5 h-5 text-greenButtons" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                            <svg class="w-5 h-5 text-greenButtons" aria-hidden="true" fill="currentColor"
+                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
                             <span class="sr-only">Search icon</span>
                         </div>
-                        <input type="text" id="search-navbar" class="placeholder-greenButtons block w-80 px-2 pl-11 font-medium text-sm text-greenButtons rounded-xl bg-appBackground border-none focus:ring-green-500 focus:border-green-500" placeholder="Search">
+                        <input type="text" id="search-navbar"
+                            class="placeholder-greenButtons block w-80 px-2 pl-11 font-medium text-sm text-greenButtons rounded-xl bg-appBackground border-none focus:ring-green-500 focus:border-green-500"
+                            placeholder="Search">
                     </div>
                 </div>
             </div>
@@ -61,13 +77,16 @@
                 <div class="hidden sm:flex sm:items-center">
                     <x-dropdown width="48">
                         <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                <img class="w-10 h-10 rounded-full border-greenButtons border-2" src="{{$user->profile_pic ? asset('storage/' . $user->profile_pic) : asset('img/logo.png')}}" alt="Rounded avatar">
+                            <button
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                <img class="w-10 h-10 rounded-full border-greenButtons border-2"
+                                    src="{{ $user->profile_pic ? asset('storage/' . $user->profile_pic) : asset('img/logo.png') }}"
+                                    alt="Rounded avatar">
                             </button>
                         </x-slot>
 
                         <x-slot name="content">
-                            <x-dropdown-link href="/users/{{$user->id}}">
+                            <x-dropdown-link href="/users/{{ $user->id }}">
                                 {{ __('Profile') }}
                             </x-dropdown-link>
 
@@ -76,7 +95,7 @@
                                 @csrf
 
                                 <x-dropdown-link :href="route('logout')"
-                                        onclick="event.preventDefault();
+                                    onclick="event.preventDefault();
                                                     this.closest('form').submit();">
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
@@ -88,10 +107,14 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2.5 rounded-md text-greenButtons hover:text-green-500 hover:bg-green-100 focus:outline-none focus:bg-green-100 focus:text-green-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2.5 rounded-md text-greenButtons hover:text-green-500 hover:bg-green-100 focus:outline-none focus:bg-green-100 focus:text-green-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -99,7 +122,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
                 {{ __('Vacancies') }}
@@ -117,16 +140,21 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
+            <div class="px-4 flex flex-col gap-1">
                 @if ($user->is_admin)
-                    <div class="font-bold text-sm text-red-500">Admin</div>
+                    <div class="flex flex-row gap-1 items-center mt-3 bg-red-800 p-1 rounded-md w-max">
+                        <span class="material-symbols-rounded text-white">
+                            admin_panel_settings
+                        </span>
+                        <p class="text-xs text-white font-bold text-center">Administrator</p>
+                    </div>
                 @endif
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->username }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link href="/users/{{$user->id}}">
+                <x-responsive-nav-link href="/users/{{ $user->id }}">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
@@ -134,7 +162,7 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
