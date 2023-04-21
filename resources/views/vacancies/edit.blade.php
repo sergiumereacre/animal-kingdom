5b4ad0a285ef3f77b19d1c02764f453d7fedf185
TheCaister <edisoncai2001@gmail.com>
2023-04-21 23:28:51 +0100

    can change some requirements

--- resources/views/vacancies/edit.blade.php
+++ resources/views/vacancies/edit.blade.php
@@ -1,22 +1,22 @@
 <x-app-layout>
-<div class="py-12 flex flex-col items-center max-w-2xl mx-auto">
+    <div class="py-12 flex flex-col items-center max-w-2xl mx-auto">
         <x-card-base>
-            <form method="POST" action="/vacancies/{{$vacancy->vacancy_id}}" enctype="multipart/form-data" onsubmit="return validateForm()"
-                class="p-10 flex flex-col items-center gap-5">
+            <form method="POST" action="/vacancies/{{ $vacancy->vacancy_id }}" enctype="multipart/form-data"
+                onsubmit="return validateForm()" class="p-10 flex flex-col items-center gap-5">
                 @csrf
                 @method('PUT')
                 <!-- Form Title -->
                 <div>
-                    <p class="font-bold text-2xl md:text-3xl">Edit Vacancy: {{$vacancy->vacancy_title}} </p>
+                    <p class="font-bold text-2xl md:text-3xl">Edit Vacancy: {{ $vacancy->vacancy_title }} </p>
                 </div>
 
-                <input type="hidden" name="organisation_id" value="{{$vacancy->organisation_id}}">
+                <input type="hidden" name="organisation_id" value="{{ $vacancy->organisation_id }}">
 
                 <div class="flex flex-col gap-5 md:w-full">
                     <div>
                         <x-input-label for="vacancy_title">Vacancy Title</x-input-label>
                         <x-text-input type="text" name="vacancy_title" class="mt-1 block w-full" placeholder=""
-                            value="{{ $vacancy->vacancy_title}}"></x-text-input>
+                            value="{{ $vacancy->vacancy_title }}"></x-text-input>
                         <x-input-error class="mt-2" :messages="$errors->get('vacancy_title')" />
                     </div>
 
@@ -24,20 +24,32 @@
                     <div>
                         <x-input-label for="category_requirement">Category Requirement</x-input-label>
                         <x-select name="category_requirement" id="category_requirement" class="mt-1 block w-full">
-@php
-    // Generate a list of all the categories, and set the selected one to the current category
-    $categories = array("MAMMAL", "REPTILE", "AMPHIBIAN", "AVIAN", "FISH");
-    // $selectedCategory = $vacancy->category_requirement;
-
-@endphp
+                            @php
+                                // Generate a list of all the categories, and set the selected one to the current category
+                                $categories = ['MAMMAL', 'REPTILE', 'AMPHIBIAN', 'AVIAN', 'FISH'];
+                                
+                                // list of eating styles
+                                $eating_styles = ['HERBIVORE', 'CARNIVORE', 'OMNIVORE'];
+                                
+                                // list of size_requirements
+                                $size_requirements = ['SMALL', 'MEDIUM', 'LARGE'];
+                                
+                                // List of speed_requirements
+                                $speed_requirements = ['SLOW', 'MEDIUM', 'FAST'];
+                                
+                                // list of num_appendages_requirements
+                                $num_appendages_requirements = ['NONE', 'FEW', 'MANY'];
+                                
+                            @endphp
 
                             <option value="NULL">None</option>
 
                             @foreach ($categories as $category)
                                 @if ($category == $vacancy->category_requirement)
-                                    <option value="{{$category}}" selected>{{ucfirst(strtolower($category))}}</option>
+                                    <option value="{{ $category }}" selected>{{ ucfirst(strtolower($category)) }}
+                                    </option>
                                 @else
-                                    <option value="{{$category}}">{{ucfirst(strtolower($category))}}</option>
+                                    <option value="{{ $category }}">{{ ucfirst(strtolower($category)) }}</option>
                                 @endif
                             @endforeach
                         </x-select>
@@ -47,9 +59,24 @@
                     <div>
                         <x-input-label for="can_fly_requirement">Should applicants be able to fly?</x-input-label>
                         <x-select name="can_fly_requirement" id="can_fly_requirement" class="mt-1 block w-full">
-                            <option value="NULL">Doesn't matter</option>
-                            <option value="1">Yes</option>
-                            <option value="0">No</option>
+
+                            @if ($vacancy->can_fly_requirement == 'NULL')
+                                <option value="NULL" selected>Doesn't matter</option>
+                            @else
+                                <option value="NULL">Doesn't matter</option>
+                            @endif
+                            @if ($vacancy->can_fly_requirement == '1')
+                                <option value="1" selected>Yes</option>
+                            @else
+                                <option value="1">Yes</option>
+                            @endif
+                            @if ($vacancy->can_fly_requirement == '0')
+                                <option value="0" selected>No</option>
+                            @else
+                                <option value="0">No</option>
+                            @endif
+
+
                         </x-select>
                         <x-input-error class="mt-2" :messages="$errors->get('can_fly_requirement')" />
                     </div>
@@ -57,9 +84,22 @@
                     <div>
                         <x-input-label for="can_swim_requirement">Should applicants be able to swim?</x-input-label>
                         <x-select name="can_swim_requirement" id="can_swim_requirement" class="mt-1 block w-full">
-                            <option value="NULL">Doesn't matter</option>
-                            <option value="1">Yes</option>
-                            <option value="0">No</option>
+
+                            @if ($vacancy->can_swim_requirement == 'NULL')
+                                <option value="NULL" selected>Doesn't matter</option>
+                            @else
+                                <option value="NULL">Doesn't matter</option>
+                            @endif
+                            @if ($vacancy->can_swim_requirement == '1')
+                                <option value="1" selected>Yes</option>
+                            @else
+                                <option value="1">Yes</option>
+                            @endif
+                            @if ($vacancy->can_swim_requirement == '0')
+                                <option value="0" selected>No</option>
+                            @else
+                                <option value="0">No</option>
+                            @endif
                         </x-select>
                         <x-input-error class="mt-2" :messages="$errors->get('can_swim_requirement')" />
                     </div>
@@ -67,9 +107,22 @@
                     <div>
                         <x-input-label for="can_climb_requirement">Should applicants be able to climb?</x-input-label>
                         <x-select name="can_climb_requirement" id="can_climb_requirement" class="mt-1 block w-full">
-                            <option value="NULL">Doesn't matter</option>
-                            <option value="1">Yes</option>
-                            <option value="0">No</option>
+
+                            @if ($vacancy->can_climb_requirement == 'NULL')
+                                <option value="NULL" selected>Doesn't matter</option>
+                            @else
+                                <option value="NULL">Doesn't matter</option>
+                            @endif
+                            @if ($vacancy->can_climb_requirement == '1')
+                                <option value="1" selected>Yes</option>
+                            @else
+                                <option value="1">Yes</option>
+                            @endif
+                            @if ($vacancy->can_climb_requirement == '0')
+                                <option value="0" selected>No</option>
+                            @else
+                                <option value="0">No</option>
+                            @endif
                         </x-select>
                         <x-input-error class="mt-2" :messages="$errors->get('can_climb_requirement')" />
                     </div>
@@ -78,10 +131,18 @@
                         <x-input-label for="eating_style_requirement">Eating Style Requirement</x-input-label>
                         <x-select name="eating_style_requirement" id="eating_style_requirement"
                             class="mt-1 block w-full">
-                            <option value="NULL">None</option>
-                            <option value="HERBIVORE">Herbivore</option>
-                            <option value="CARNIVORE">Carnivore</option>
-                            <option value="OMNIVORE">Omnivore</option>
+                            <option value="NULL">Doesn't matter</option>
+
+                            @foreach ($eating_styles as $eating_style)
+                                @if ($eating_style == $vacancy->eating_style_requirement)
+                                    <option value="{{ $eating_style }}" selected>
+                                        {{ ucfirst(strtolower($eating_style)) }}
+                                    </option>
+                                @else
+                                    <option value="{{ $eating_style }}">{{ ucfirst(strtolower($eating_style)) }}
+                                    </option>
+                                @endif
+                            @endforeach
                         </x-select>
                         <x-input-error class="mt-2" :messages="$errors->get('eating_style_requirement')" />
                     </div>
@@ -91,9 +152,22 @@
                         </x-input-label>
                         <x-select name="produces_toxins_requirement" id="produces_toxins_requirement"
                             class="mt-1 block w-full">
-                            <option value="NULL">Doesn't matter</option>
-                            <option value="1">Yes</option>
-                            <option value="0">No</option>
+
+                            @if ($vacancy->produces_toxins_requirement == 'NULL')
+                                <option value="NULL" selected>Doesn't matter</option>
+                            @else
+                                <option value="NULL">Doesn't matter</option>
+                            @endif
+                            @if ($vacancy->produces_toxins_requirement == '1')
+                                <option value="1" selected>Yes</option>
+                            @else
+                                <option value="1">Yes</option>
+                            @endif
+                            @if ($vacancy->produces_toxins_requirement == '0')
+                                <option value="0" selected>No</option>
+                            @else
+                                <option value="0">No</option>
+                            @endif
                         </x-select>
                         <x-input-error class="mt-2" :messages="$errors->get('produces_toxins_requirement')" />
                     </div>
@@ -102,9 +176,16 @@
                         <x-input-label for="size_requirement">Size Requirement</x-input-label>
                         <x-select name="size_requirement" id="size_requirement" class="mt-1 block w-full">
                             <option value="NULL">Doesn't matter</option>
-                            <option value="SMALL">Small</option>
-                            <option value="MEDIUM">Medium</option>
-                            <option value="LARGE">Large</option>
+
+                            @foreach ($size_requirements as $size)
+                                @if ($size == $vacancy->size_requirement)
+                                    <option value="{{ $size }}" selected>{{ ucfirst(strtolower($size)) }}
+                                    </option>
+                                @else
+                                    <option value="{{ $size }}">{{ ucfirst(strtolower($size)) }}</option>
+                                @endif
+                            @endforeach
+
                         </x-select>
                         <x-input-error class="mt-2" :messages="$errors->get('size_requirement')" />
                     </div>
@@ -113,9 +194,15 @@
                         <x-input-label for="speed_requirement">Speed Requirement</x-input-label>
                         <x-select name="speed_requirement" id="speed_requirement" class="mt-1 block w-full">
                             <option value="NULL">Doesn't matter</option>
-                            <option value="SLOW">Slow</option>
-                            <option value="MEDIUM">Medium</option>
-                            <option value="FAST">Fast</option>
+
+                            @foreach ($speed_requirements as $speed)
+                                @if ($speed == $vacancy->speed_requirement)
+                                    <option value="{{ $speed }}" selected>{{ ucfirst(strtolower($speed)) }}
+                                    </option>
+                                @else
+                                    <option value="{{ $speed }}">{{ ucfirst(strtolower($speed)) }}</option>
+                                @endif
+                            @endforeach
                         </x-select>
                         <x-input-error class="mt-2" :messages="$errors->get('speed_requirement')" />
                     </div>
@@ -138,7 +225,7 @@
                             with colon and
                             separate with commas.</x-input-label>
                         <x-text-input id="skills_list" type="text" name="skills" class="mt-1 block w-full"
-                            placeholder="" value="{{$vacancy->skills}}"></x-text-input>
+                            placeholder="" value="{{ $vacancy->skills }}"></x-text-input>
                     </div>
 
                     <div class="flex flex-col gap-5">
@@ -200,7 +287,7 @@
                             with
                             commas.</x-input-label>
                         <x-text-input id="qualifications_list" type="text" name="qualifications"
-                            class="mt-1 block w-full" placeholder="" value="{{ $vacancy->qualifications}}">
+                            class="mt-1 block w-full" placeholder="" value="{{ $vacancy->qualifications }}">
                         </x-text-input>
                     </div>
                     <div class="flex flex-col gap-5">
@@ -211,8 +298,7 @@
 
                         <div>
                             <x-input-label for="qualifications_select">Select qualifications here</x-input-label>
-                            <x-select id="qualification_name" name="qualifications_select"
-                                class="mt-1 block w-full">
+                            <x-select id="qualification_name" name="qualifications_select" class="mt-1 block w-full">
                                 @foreach ($qualifications as $qualification)
                                     <option value="{{ $qualification->qualification_name }}">
                                         {{ $qualification->qualification_name }}
@@ -222,7 +308,9 @@
                         </div>
 
                         <div class="flex flex-col items-center">
-                            <p id="qualifications_error" hidden class="text-center text-white bg-redButtons w-max px-2 rounded-lg">Can't have duplicate qualifications.</p>
+                            <p id="qualifications_error" hidden
+                                class="text-center text-white bg-redButtons w-max px-2 rounded-lg">Can't have duplicate
+                                qualifications.</p>
                         </div>
 
                         <div class="flex flex-col items-center">
@@ -252,13 +340,17 @@
                     {{-- MUST CHECK IF SALARY RANGE IS LOWER --}}
                     <div>
                         <x-input-label for="salary_range_lower">Lower salary range</x-input-label>
-                        <x-text-input id="salary_lower" type="number" name="salary_range_lower" class="mt-1 block w-full" placeholder="" value="{{ $vacancy->salary_range_lower }}" required></x-text-input>
+                        <x-text-input id="salary_lower" type="number" name="salary_range_lower"
+                            class="mt-1 block w-full" placeholder="" value="{{ $vacancy->salary_range_lower }}"
+                            required></x-text-input>
                         <x-input-error class="mt-2" :messages="$errors->get('salary_range_lower')" />
                     </div>
 
                     <div>
                         <x-input-label for="salary_range_upper">Upper salary range</x-input-label>
-                        <x-text-input id="salary_upper" type="number" name="salary_range_upper" class="mt-1 block w-full" placeholder="" value="{{ $vacancy->salary_range_upper }}" required></x-text-input>
+                        <x-text-input id="salary_upper" type="number" name="salary_range_upper"
+                            class="mt-1 block w-full" placeholder="" value="{{ $vacancy->salary_range_upper }}"
+                            required></x-text-input>
                         <x-input-error class="mt-2" :messages="$errors->get('salary_range_upper')" />
                     </div>
 
@@ -280,7 +372,9 @@
 
                     <div>
                         <x-input-label for="vacancy_description">Vacancy Description</x-input-label>
-                        <textarea name="vacancy_description" id="vacancy_description" rows="10" class="mt-1 block w-full border-gray-300 focus:border-greenButtons focus:ring-greenButtons rounded-lg shadow-md" placeholder="Description Details.">{{ $vacancy->vacacny_description }}</textarea>
+                        <textarea name="vacancy_description" id="vacancy_description" rows="10"
+                            class="mt-1 block w-full border-gray-300 focus:border-greenButtons focus:ring-greenButtons rounded-lg shadow-md"
+                            placeholder="Description Details.">{{ $vacancy->vacacny_description }}</textarea>
                         <x-input-error class="mt-2" :messages="$errors->get('vacancy_description')" />
                     </div>
 
